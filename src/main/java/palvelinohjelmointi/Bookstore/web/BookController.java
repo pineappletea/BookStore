69f6df0cd5386b301889a456391397bf1153bb2a
pineappletea <47184314+pineappletea@users.noreply.github.com>
2020-02-12 20:39:53 +0200

    Ensivienti

---
+++ src/main/java/palvelinohjelmointi/Bookstore/web/BookController.java
@@ -0,0 +1,18 @@
+package palvelinohjelmointi.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class BookController {
+
+	@RequestMapping(value="/index", method = RequestMethod.GET)
+	public String placeHolderResponse() {
+
+		return "index";
+	}
+}
